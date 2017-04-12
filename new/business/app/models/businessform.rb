1c6690af8c68b32a4fe6400726e41e2ddc7350f2
shiv <shiva.shivjamesh@gmail.com>
2017-04-12 16:48:17 +0530

    New error fix

--- new/business/app/models/businessform.rb
+++ new/business/app/models/businessform.rb
@@ -1,8 +1,8 @@
 class Businessform < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :category
-  validates :name, :street, :state, :postcode, :website, :country, :phonenumber, :category_id, :description, :city, presence: true
-  validates :description, :length => { :minimum => 30 }
+  validates :name, :street, :state, :postcode, :website, :country, :phone, :category_id, :description, :city, presence: true
+  validates :description, :length => { :minimum => 30, :maximum => 200 }
 
 
 end
